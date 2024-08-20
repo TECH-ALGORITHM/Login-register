3ae2eda551047a64414f3e411841ac903e1123c9
AMBUJ SINGH <joy212ambuj@gmail.com>
2024-08-20 21:23:05 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const FormDataModel = require("./models/FormData");
+const bcrypt = require("bcrypt");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect("mongodb://127.0.0.1:27017/delote");
+
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await FormDataModel.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already registered" });
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    d;
+    const newUser = await FormDataModel.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+
+    res.json(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error registering user" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await FormDataModel.findOne({ email });
+
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (isMatch) {
+        res.json("Success");
+      } else {
+        res.status(400).json("Wrong password");
+      }
+    } else {
+      res.status(404).json("No records found!");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error logging in" });
+  }
+});
